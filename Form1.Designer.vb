4f4b7596fc1f44741d9b345fe6f505e0f26c0b56
joshu <joshuacarpentier21@gmail.com>
2020-02-05 16:33:44 -0800

    fixed code

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,35 +23,18 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.btnLoginIn = New System.Windows.Forms.Button()
-        Me.txtnum = New System.Windows.Forms.TextBox()
-        Me.txtsum = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'btnLoginIn
         '
         Me.btnLoginIn.BackColor = System.Drawing.Color.DarkBlue
         Me.btnLoginIn.ForeColor = System.Drawing.SystemColors.HighlightText
-        Me.btnLoginIn.Location = New System.Drawing.Point(48, 67)
+        Me.btnLoginIn.Location = New System.Drawing.Point(50, 279)
         Me.btnLoginIn.Name = "btnLoginIn"
         Me.btnLoginIn.Size = New System.Drawing.Size(75, 40)
         Me.btnLoginIn.TabIndex = 4
         Me.btnLoginIn.Text = "Generate Buttons"
         Me.btnLoginIn.UseVisualStyleBackColor = False
-        '
-        'txtnum
-        '
-        Me.txtnum.Location = New System.Drawing.Point(48, 28)
-        Me.txtnum.Name = "txtnum"
-        Me.txtnum.Size = New System.Drawing.Size(75, 20)
-        Me.txtnum.TabIndex = 6
-        '
-        'txtsum
-        '
-        Me.txtsum.Location = New System.Drawing.Point(202, 28)
-        Me.txtsum.Name = "txtsum"
-        Me.txtsum.Size = New System.Drawing.Size(100, 20)
-        Me.txtsum.TabIndex = 7
-        Me.txtsum.Text = "0"
         '
         'Form1
         '
@@ -59,16 +42,11 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(913, 350)
-        Me.Controls.Add(Me.txtsum)
-        Me.Controls.Add(Me.txtnum)
         Me.Controls.Add(Me.btnLoginIn)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
     Friend WithEvents btnLoginIn As Button
-    Friend WithEvents txtnum As TextBox
-    Friend WithEvents txtsum As TextBox
 End Class
--- Form1.vb
+++ Form1.vb
@@ -4,30 +4,30 @@ Public Class Form1
 
     Private Sub xyz(sender As Object, e As EventArgs) Handles btnLoginIn.Click
         Dim Xaxis As Integer = 8
-        Dim y As Integer = 8
+        Dim Yaxis As Integer = 8
         Dim blue As Boolean = True
 
         For i = 1 To 8
-            x = 8
+            Xaxis = 8
             If i Mod 2 = 0 Then
                 blue = False
             Else
                 blue = True
             End If
             For j = 1 To 8
-                Dim newlabel As Label
-                newlabel = New System.Windows.Forms.Label()
+                Dim Lblsquare As Label
+                Lblsquare = New System.Windows.Forms.Label()
                 If blue = True Then
-                    newlabel.BackColor = System.Drawing.Color.Aqua
+                    Lblsquare.BackColor = System.Drawing.Color.Aqua
                 ElseIf blue = False Then
-                    newlabel.BackColor = System.Drawing.Color.Black
+                    Lblsquare.BackColor = System.Drawing.Color.Black
                 End If
 
-                newlabel.Location = New System.Drawing.Point(Xaxis, Yaxis)
-                newlabel.Size = New System.Drawing.Size(30, 30)
-                newlabel.Size = New System.Drawing.Size(30, 30)
+                Lblsquare.Location = New System.Drawing.Point(Xaxis, Yaxis)
+                Lblsquare.Size = New System.Drawing.Size(30, 30)
+                Lblsquare.Size = New System.Drawing.Size(30, 30)
 
-                Me.Controls.Add(newlabel)
+                Me.Controls.Add(Lblsquare)
                 Xaxis = Xaxis + 30
                 blue = Not blue
             Next
