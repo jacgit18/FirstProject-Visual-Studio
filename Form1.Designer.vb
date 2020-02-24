67b7ec7f9c64bbd73fcfe1638aeec901f6699b2f
jac <joshuacarpentier21@gmail.com>
2020-02-24 16:37:57 -0500

    added pic

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,128 +22,44 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.greeting = New System.Windows.Forms.Label()
-        Me.txtUsername = New System.Windows.Forms.TextBox()
-        Me.txtPassword = New System.Windows.Forms.TextBox()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.btnLoginIn = New System.Windows.Forms.Button()
-        Me.lblMessage = New System.Windows.Forms.Label()
-        Me.txtnum = New System.Windows.Forms.TextBox()
-        Me.txtsum = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Label1
+        'PictureBox1
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.BackColor = System.Drawing.Color.Khaki
-        Me.Label1.ForeColor = System.Drawing.SystemColors.Highlight
-        Me.Label1.Location = New System.Drawing.Point(57, 35)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(55, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Username"
+        Me.PictureBox1.Image = Global.FirstProject.My.Resources.Resources.citytech_logo
+        Me.PictureBox1.Location = New System.Drawing.Point(288, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(296, 90)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
         '
-        'greeting
+        'Button1
         '
-        Me.greeting.AutoSize = True
-        Me.greeting.BackColor = System.Drawing.Color.Khaki
-        Me.greeting.ForeColor = System.Drawing.SystemColors.Highlight
-        Me.greeting.Location = New System.Drawing.Point(57, 5)
-        Me.greeting.Name = "greeting"
-        Me.greeting.Size = New System.Drawing.Size(52, 13)
-        Me.greeting.TabIndex = 0
-        Me.greeting.Text = "Greetings"
-        '
-        'txtUsername
-        '
-        Me.txtUsername.Location = New System.Drawing.Point(140, 35)
-        Me.txtUsername.Name = "txtUsername"
-        Me.txtUsername.Size = New System.Drawing.Size(100, 20)
-        Me.txtUsername.TabIndex = 1
-        '
-        'txtPassword
-        '
-        Me.txtPassword.Location = New System.Drawing.Point(140, 79)
-        Me.txtPassword.Name = "txtPassword"
-        Me.txtPassword.Size = New System.Drawing.Size(100, 20)
-        Me.txtPassword.TabIndex = 3
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.BackColor = System.Drawing.Color.Khaki
-        Me.Label2.ForeColor = System.Drawing.SystemColors.Highlight
-        Me.Label2.Location = New System.Drawing.Point(57, 79)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(53, 13)
-        Me.Label2.TabIndex = 2
-        Me.Label2.Text = "Password"
-        '
-        'btnLoginIn
-        '
-        Me.btnLoginIn.BackColor = System.Drawing.Color.DarkBlue
-        Me.btnLoginIn.ForeColor = System.Drawing.SystemColors.HighlightText
-        Me.btnLoginIn.Location = New System.Drawing.Point(116, 140)
-        Me.btnLoginIn.Name = "btnLoginIn"
-        Me.btnLoginIn.Size = New System.Drawing.Size(75, 40)
-        Me.btnLoginIn.TabIndex = 4
-        Me.btnLoginIn.Text = "Generate Buttons"
-        Me.btnLoginIn.UseVisualStyleBackColor = False
-        '
-        'lblMessage
-        '
-        Me.lblMessage.AutoSize = True
-        Me.lblMessage.Location = New System.Drawing.Point(41, 187)
-        Me.lblMessage.Name = "lblMessage"
-        Me.lblMessage.Size = New System.Drawing.Size(39, 13)
-        Me.lblMessage.TabIndex = 5
-        Me.lblMessage.Text = "Label3"
-        '
-        'txtnum
-        '
-        Me.txtnum.Location = New System.Drawing.Point(215, 151)
-        Me.txtnum.Name = "txtnum"
-        Me.txtnum.Size = New System.Drawing.Size(56, 20)
-        Me.txtnum.TabIndex = 6
-        '
-        'txtsum
-        '
-        Me.txtsum.Location = New System.Drawing.Point(381, 151)
-        Me.txtsum.Name = "txtsum"
-        Me.txtsum.Size = New System.Drawing.Size(100, 20)
-        Me.txtsum.TabIndex = 7
-        Me.txtsum.Text = "0"
+        Me.Button1.Location = New System.Drawing.Point(380, 138)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.Color.White
+        Me.BackColor = System.Drawing.Color.Black
         Me.ClientSize = New System.Drawing.Size(661, 209)
-        Me.Controls.Add(Me.txtsum)
-        Me.Controls.Add(Me.txtnum)
-        Me.Controls.Add(Me.lblMessage)
-        Me.Controls.Add(Me.btnLoginIn)
-        Me.Controls.Add(Me.txtPassword)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.txtUsername)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.greeting)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Label1 As Label
-    Friend WithEvents greeting As Label
-    Friend WithEvents txtUsername As TextBox
-    Friend WithEvents txtPassword As TextBox
-    Friend WithEvents Label2 As Label
-    Friend WithEvents btnLoginIn As Button
-    Friend WithEvents lblMessage As Label
-    Friend WithEvents txtnum As TextBox
-    Friend WithEvents txtsum As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,46 +1,9 @@
 ﻿Option Strict Off
 
 Public Class Form1
-    Private Sub xyz(sender As Object, e As EventArgs) Handles btnLoginIn.Click, Label1.Click
-
-        If txtUsername.Text.ToLower = txtPassword.Text.ToLower Then
-            lblMessage.Text = "success"
-        Else
-            lblMessage.Text = "not success"
-        End If
-
-        ' Responsible for deleting buttons
-        For j = Me.Controls.Count - 1 To 0 Step -1
-            If Me.Controls(j).Name.Length > 10 AndAlso Me.Controls(j).Name.Substring(0, 10) = "New Button" Then
-                Me.Controls.RemoveAt(j)
-            End If
-        Next
-        'Runtime code
-        For i = 1 To Integer.Parse(txtnum.Text)
-            'MessageBox.Show("making button " + i.ToString)
-            Dim nextbutton As Button
-            nextbutton = New System.Windows.Forms.Button()
-            nextbutton.BackColor = System.Drawing.Color.DarkBlue
-            nextbutton.ForeColor = System.Drawing.SystemColors.HighlightText
-            nextbutton.Location = New System.Drawing.Point(116, 140 + (i * 45))
-            nextbutton.Name = "New Button " + i.ToString
-            nextbutton.Size = New System.Drawing.Size(75, 40)
-            nextbutton.Text = "Button " + i.ToString
-            AddHandler nextbutton.Click, AddressOf newbuttonclick
-            Me.Controls.Add(nextbutton)
-        Next
-        Me.Size = New System.Drawing.Size(Me.Size.Width, 245 + (Integer.Parse(txtnum.Text) * 45))
-    End Sub
-    Private Sub newbuttonclick(sender As Object, e As EventArgs)
-        '  MessageBox.Show("a button was clicked")
-        Dim clickbutton As Button
-        ' casting
-        clickbutton = CType(sender, Button)
-        ' MessageBox.Show(clickbutton.Name)
-        Dim number As Integer
-        number = clickbutton.Name.Substring(11)
-        txtsum.Text = txtsum.Text + number
+    Private Sub xyz(sender As Object, e As EventArgs)
 
     End Sub
+
 
 End Class
